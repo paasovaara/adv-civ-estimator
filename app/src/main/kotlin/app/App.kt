02b00ddd9c49a61f5efcd89dd25950502574e406
Markus Paasovaara <markus.paasovaara@barona.fi>
2022-08-02 16:48:15 +0300

    Minor refactoring

--- app/src/main/kotlin/app/App.kt
+++ app/src/main/kotlin/app/App.kt
@@ -3,27 +3,36 @@ package app
 import kotlinx.cli.*
 
 class App {
-    val appName = "Advanced Civ Estimator"
-    val version = "0.0.1"
+    companion object {
+        val appName = "Advanced Civ Estimator"
+        val version = "0.0.1"
+        val greeting: String
+            get() {
+                return  "     _       _                               _    ____ _        \n" +
+                        "    / \\   __| |_   ____ _ _ __   ___ ___  __| |  / ___(_)_   __ \n" +
+                        "   / _ \\ / _` \\ \\ / / _` | '_ \\ / __/ _ \\/ _` | | |   | \\ \\ / / \n" +
+                        "  / ___ \\ (_| |\\ V / (_| | | | | (_|  __/ (_| | | |___| |\\ V /  \n" +
+                        " /_/   \\_\\__,_| \\_/ \\__,_|_| |_|\\___\\___|\\__,_|  \\____|_| \\_/   \n" +
+                        "                                                                "
+            }
+    }
 
-    val greeting: String
-        get() {
-            return  "     _       _                               _    ____ _        \n" +
-                    "    / \\   __| |_   ____ _ _ __   ___ ___  __| |  / ___(_)_   __ \n" +
-                    "   / _ \\ / _` \\ \\ / / _` | '_ \\ / __/ _ \\/ _` | | |   | \\ \\ / / \n" +
-                    "  / ___ \\ (_| |\\ V / (_| | | | | (_|  __/ (_| | | |___| |\\ V /  \n" +
-                    " /_/   \\_\\__,_| \\_/ \\__,_|_| |_|\\___\\___|\\__,_|  \\____|_| \\_/   \n" +
-                    "                                                                "
-        }
+    fun launch() {
+
+    }
 }
 
 fun main(args: Array<String>) {
-    val app = App()
-    println(app.greeting)
+    println(App.greeting)
 
-    val parser = ArgParser("${app.appName}:: ${app.version}")
+    val parser = ArgParser("${App.appName}:: ${App.version}")
     val version by parser.option(ArgType.Boolean, shortName = "v", description = "Version").default(false)
 
     parser.parse(args)
-    if (version) println(app.version)
+    if (version) {
+        println(App.version)
+    } else {
+        val app = App()
+        app.launch()
+    }
 }
